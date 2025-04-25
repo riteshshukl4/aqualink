f109f43f10e4cb1d69268382637c452b21a9a273
Ritesh Shukla <riteshshuklamail@gmail.com>
2025-04-25 18:16:50 +0000

    module.exports = {   content: ["./src/**/*.{js,jsx,ts,tsx}"],   theme: {     extend: {       fontFamily: {         sans: ['Inter', 'sans-serif'],       },     },   },   plugins: [require('@tailwindcss/forms')], } add this js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+        },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -114,5 +117,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/forms')],
 } satisfies Config;
